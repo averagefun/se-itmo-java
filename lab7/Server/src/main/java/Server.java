6f35461635cb2c7be842f264d091b7b5fef7a0ef
Davie506 <abjw03@gmail.com>
2022-04-26 01:28:32 +0300

    lab 7: create server shell

--- lab7/Server/src/main/java/Server.java
+++ lab7/Server/src/main/java/Server.java
@@ -16,6 +16,8 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,46 +61,93 @@ public class Server {
     }
     
     public void start() {
-        log.info("successfully started at port {}", Common.PORT);
+        log.info("RECEIVER -> STARTED AT PORT {}", Common.PORT);
         while (true) {
             try {
                 receiveAndAnswer();
+                if (Thread.currentThread().isInterrupted()) {
+                    log.info("RECEIVER -> SHUT DOWN");
+                    break;
+                }
             } catch (IOException | ClassNotFoundException | InterruptedException e) {
-                e.printStackTrace();
+                log.error("receive-answer method:\n{}", MyExceptions.getStringStackTrace(e));
                 break;
             }
         }
     }
 
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) {
         JsonParser jp = new JsonParser();
-        HashMap<String, String> config = new HashMap<>();
+        HashMap<String, String> config;
         try {
             String configFile = "config.json";
             if (args.length > 0 && args[0].equals("helios")) configFile = "config_helios.json";
             String text = new FileManager().readResourcesFile(configFile);
             config = jp.jsonToMap(text);
+            log.info("successfully loaded config file '{}'", configFile);
         } catch (IOException | JsonSyntaxException e) {
             log.error("failed to load config file:\n{}", MyExceptions.getStringStackTrace(e));
-            System.exit(0);
+            return;
         }
 
-        Database db = null;
-        try {
-            db = new Database(config.get("host"), config.get("db_name"),
-                    config.get("user"), config.get("password"), config.get("db_salt"));
-        } catch (SQLException e) {
-            log.error("database connection error:\n{}", MyExceptions.getStringStackTrace(e));
-            System.exit(0);
+        Thread receiver = new Thread(() -> {
+            Database db;
+            try {
+                db = new Database(config.get("host"), config.get("db_name"),
+                        config.get("user"), config.get("password"), config.get("db_salt"));
+                log.info("successfully connected to a database '{}'", config.get("db_name"));
+            } catch (SQLException e) {
+                log.error("database connection error:\n{}", MyExceptions.getStringStackTrace(e));
+                return;
+            }
+
+            MovieCollection mc = new MovieCollection(db);
+            CommandManager cm = new CommandManager(mc, db);
+
+            DatagramSocket datagramSocket;
+            try {
+                datagramSocket = new DatagramSocket(Common.PORT);
+            } catch (SocketException e) {
+                log.error("problem with DatagramSocket on port {}:\n{}", Common.PORT, MyExceptions.getStringStackTrace(e));
+                return;
+            }
+
+            Server server = new Server(datagramSocket, cm);
+            server.start();
+        });
+
+        Scanner sc = new Scanner(System.in);
+        interactiveMode(sc, receiver);
+    }
+
+    public static void interactiveMode(Scanner sc, Thread receiver) {
+        log.info("successfully started server shell: available commands: 'start', 'stop'.\nStarting receiver by default...");
+        receiver.start();
+        while(true) {
+            String input = "";
+            try {
+                input = sc.nextLine().trim();
+            } catch (NoSuchElementException e) {
+                System.exit(0);
+            }
+
+            switch (input) {
+                case "start":
+                    if (receiver.isAlive()) {
+                        log.warn("already started");
+                        continue;
+                    }
+                    receiver.start();
+                    break;
+                case "stop":
+                    if (!receiver.isAlive()) {
+                        log.warn("server already shut down");
+                        continue;
+                    }
+                    receiver.interrupt();
+                    break;
+            }
         }
-
-        MovieCollection mc = new MovieCollection(db);
-        CommandManager cm = new CommandManager(mc, db);
-
-        DatagramSocket datagramSocket = new DatagramSocket(Common.PORT);
-
-        Server server = new Server(datagramSocket, cm);
-        server.start();
     }
 
 }
